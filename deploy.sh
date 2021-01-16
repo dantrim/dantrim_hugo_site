2f2610b66818d907553b32658224c1fbde51af28
dantrim <dantrim1023@gmail.com>
2021-01-15 16:09:59 -0800

    update deploy script to push public to repo

--- deploy.sh
+++ deploy.sh
@@ -8,18 +8,28 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 # Build the project.
 hugo -t LoveIt # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-# Go To Public folder
-cd public
+if [ -d public ]; then
+    
+    # Go To Public folder
+    pushd public
+    
+    # Add changes to git.
+    git add .
+    
+    # Commit changes.
+    msg="rebuilding site $(date)"
+    if [ -n "$*" ]; then
+    	msg="$*"
+    fi
+    git commit -m "$msg"
+    
+    # Push source and build repos.
+    git push origin main
+    popd
 
-# Add changes to git.
-git add .
-
-# Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
+    git add public
+    git commit -m "$msg"
+    git push
+else
+    printf "\033[0;31mNo \"public\" dir found!\033[0m\n"
 fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin main
